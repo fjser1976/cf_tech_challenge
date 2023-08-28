0d9382eb197052e20ae5b0efc3a77da781a52215
fjser1976 <143365536+fjser1976@users.noreply.github.com>
2023-08-27 21:24:53 -0400

    Update providers.tf

--- Terraform code/CF/providers.tf
+++ Terraform code/CF/providers.tf
@@ -1,6 +1,6 @@
 provider "aws" {
     region      = "us-east-2"
-    access_key  = "AKIAWLEQSLTTR4VOED5Q"
-    secret_key  = "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" 
+    access_key  = "CHANGEME"
+    secret_key  = "CHANGEME" 
 }
 
